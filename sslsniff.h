610c64afcd867d562b11c5c64c4fd6014fdb5ea9
dejface <48999125+dejface@users.noreply.github.com>
2020-10-14 23:01:01 +0200

    Update sslsniff.h

--- sslsniff.h
+++ sslsniff.h
@@ -31,13 +31,31 @@ using namespace std;
 #define TLS_HEADER_LEN 5
 #define HEADER_LEN 54
 
+/* struct which holds user params from commandline */
+struct userArgs {
+    string interface;
+    const char* file;
+    string ipClient, ipServer;
+    int currentPacketSize;
+    bool interfaceSet;
+    bool fileSet;
+} userArgs;
+
+typedef struct connectionInfo {
+    string ipClient, ipServer, timestamp, hostname;
+    long sec,usec;
+    unsigned srcPort, dstPort;
+    bool handshakeMade, closed;
+    int countOfPackets, length;
+} connectionInfo;
+
 void initStruct();
 int parseArgs(int argc, char** argv);
 void processPacket(u_char* args, const struct pcap_pkthdr* header, const u_char* buffer);
 string getTimestamp(const u_char* Buffer, int Size, string state);
-void printTCP(const u_char* Buffer, int Size);
-static int parseTLS(const u_char* Buffer, size_t Size);
-static int parse_extension(const uint8_t *Buffer, size_t Size);
-static int parse_server_name_extension(const uint8_t *Buffer, size_t Size);
+void printTCP(const u_char* Buffer, int Size, const struct pcap_pkthdr* header);
+static int parseTLS(const u_char* Buffer, size_t Size, const struct pcap_pkthdr* header);
+static int parse_extension(const uint8_t *Buffer, size_t Size, unsigned srcPort, unsigned dstPort);
+static int parse_server_name_extension(const uint8_t *Buffer, size_t Size, unsigned srcPort, unsigned dstPort);
 //int allocHostname(size_t len);]
 
