77931e010d70ab2cbd76b4d64f7123d6110f0419
dejface <48999125+dejface@users.noreply.github.com>
2020-11-03 19:30:23 +0100

    Update sslsniff.h

--- sslsniff.h
+++ sslsniff.h
@@ -1,3 +1,11 @@
+/****************************************************************************/
+/* File: sslsniff.h
+/* Author: David Oravec (xorave05)
+/* Description:
+/*      - header file for sslsniff.cc with libraries, macros and function
+/*        prototypes
+*****************************************************************************/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,8 +17,6 @@
 #include <arpa/inet.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
-#include <netinet/udp.h>
-#include <netinet/ip_icmp.h>
 #include <netinet/if_ether.h>
 #include <stdbool.h>
 #include <getopt.h>
@@ -44,7 +50,7 @@ typedef struct connectionInfo {
     string ipClient, ipServer, timestamp, hostname;
     long sec,usec;
     unsigned srcPort, dstPort;
-    bool handshakeMadeClient, handshakeMade,dontWrite;
+    bool handshakeMadeClient, handshakeMade,dontWrite,hasHostname,finClient, finServer;
     int countOfPackets, length, finCount, ackCount, synCount;
 } connectionInfo;
 
@@ -52,7 +58,10 @@ void initStruct();
 int parseArgs(int argc, char** argv);
 void processPacket(u_char* args, const struct pcap_pkthdr* header, const u_char* buffer);
 string getTimestamp(const u_char* Buffer, int Size, string state);
+string getDest(const u_char* Buffer, size_t Size);
+string getSource(const u_char* Buffer, size_t Size);
+void endConnection(unsigned port, string lastTimestamp, struct tcphdr* tcph, const u_char* temp, size_t Size);
 void processTCP(const u_char* Buffer, int Size, const struct pcap_pkthdr* header);
 static int parseTLS(const u_char* Buffer, size_t Size, const struct pcap_pkthdr* header);
-static int parse_extension(const uint8_t *Buffer, size_t Size, unsigned srcPort, unsigned dstPort);
-static int parse_server_name_extension(const uint8_t *Buffer, size_t Size, unsigned srcPort, unsigned dstPort);
+static int parse_extension(const uint8_t *Buffer, const uint8_t * temp, size_t Size, unsigned srcPort, unsigned dstPort, struct tcphdr* tcph);
+static int parse_server_name_extension(const uint8_t *Buffer, const uint8_t * temp, size_t Size, unsigned srcPort, unsigned dstPort, struct tcphdr* tcph);
