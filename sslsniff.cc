585ccea99a6140f675318ba090e584a8c6d28bba
dejface <48999125+dejface@users.noreply.github.com>
2020-11-03 23:52:38 +0100

    Update sslsniff.cc

--- sslsniff.cc
+++ sslsniff.cc
@@ -506,6 +506,16 @@ static int parseTLS(const u_char* Buffer, size_t Size, const struct pcap_pkthdr*
 
     /* Copyright (c) 2011 and 2012, Dustin Lundquist <dustin@null-ptr.net>
      * All rights reserved.
+     *
+     * Redistribution and use in source and binary forms, with or without
+     * modification, are permitted provided that the following conditions are met:
+     *
+     * 1. Redistributions of source code must retain the above copyright notice,
+     *    this list of conditions and the following disclaimer.
+     * 2. Redistributions in binary form must reproduce the above copyright
+     *    notice, this list of conditions and the following disclaimer in the
+     *    documentation and/or other materials provided with the distribution.
+     *
      * Following snippet of code for parsing SNI from packet was borrowed from
      * Dustin Lundquist from his tls.c module.
      * Code was modified for my purposes and license terms are included in documentation.
